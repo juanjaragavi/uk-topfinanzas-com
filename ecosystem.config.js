04e23bf9b72225de628c6da11a31f25882509d71
juanjaragavi <info@juanjaramillo.tech>
2025-05-02 13:55:09 +0000

    Some remote files updated.

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "uk-topfinanzas-com",
+    script : ".next/standalone/server.js",
+    env: {
+      PORT: 3004,
+      NODE_ENV: "production"
+    }
+  }]
+}
